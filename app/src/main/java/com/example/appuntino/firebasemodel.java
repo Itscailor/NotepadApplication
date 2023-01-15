2e0539129c61947b461b2a5fef447ba18f38a996
itscailor <l.cairo@itsrizzoli.it>
2023-01-15 20:16:40 +0100

    Colori random funzionanti

--- app/src/main/java/com/example/appuntino/firebasemodel.java
+++ app/src/main/java/com/example/appuntino/firebasemodel.java
@@ -1,4 +1,34 @@
 package com.example.appuntino;
 
 public class firebasemodel {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    private String content;
+
+    public firebasemodel(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public firebasemodel(){
+
+    }
 }
+
+
--- app/src/main/java/com/example/appuntino/notesactivity.java
+++ app/src/main/java/com/example/appuntino/notesactivity.java
@@ -1,21 +1,35 @@
 package com.example.appuntino;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class notesactivity extends AppCompatActivity {
     FloatingActionButton createnotebutt;
@@ -46,7 +60,54 @@ public class notesactivity extends AppCompatActivity {
             }
         });
 
+        Query query =firebaseFirestore.collection("notes").document(firebaseUser.getUid()).collection("myNotes").orderBy("title",Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<firebasemodel> allusernotes=new FirestoreRecyclerOptions.Builder<firebasemodel>().setQuery(query,firebasemodel.class).build();
+        noteAdapter = new FirestoreRecyclerAdapter<firebasemodel, NoteViewHolder>(allusernotes) {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            protected void onBindViewHolder(@NonNull NoteViewHolder noteViewHolder, int position, @NonNull firebasemodel firebasemodel) {
 
+                int colorcode=getRandomColor();
+                noteViewHolder.mnote.setBackgroundColor(noteViewHolder.itemView.getResources().getColor(colorcode,null));
+                noteViewHolder.notetitle.setText(firebasemodel.getTitle());
+                noteViewHolder.notecontent.setText(firebasemodel.getContent());
+
+                noteViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //open note actiity
+                        Toast.makeText(getApplicationContext(),"funziona",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notes_layout,parent,false);
+                return new NoteViewHolder(view);
+            }
+        };
+
+
+        mrecyclerView= findViewById(R.id.recyclerview);
+        mrecyclerView.setHasFixedSize(true);
+        staggeredGridLayoutManager=new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+        mrecyclerView.setLayoutManager(staggeredGridLayoutManager);
+        mrecyclerView.setAdapter(noteAdapter);
+    }
+
+
+    public class NoteViewHolder extends RecyclerView.ViewHolder{
+        private TextView notetitle;
+        private TextView notecontent;
+        LinearLayout mnote;
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            notetitle = itemView.findViewById(R.id.notetitle);
+            notecontent = itemView.findViewById(R.id.notecontent);
+            mnote = itemView.findViewById(R.id.note);
+        }
     }
 
     @Override
@@ -65,5 +126,36 @@ public class notesactivity extends AppCompatActivity {
                 startActivity(new Intent(notesactivity.this,MainActivity.class));
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.startListening();
+        if(noteAdapter!=null){
+            noteAdapter.startListening();
+        }
+    }
+
+    private int getRandomColor(){
+        List<Integer> colorcode = new ArrayList<>();
+        colorcode.add(R.color.gray);
+        colorcode.add(R.color.pink);
+        colorcode.add(R.color.lightgreen);
+        colorcode.add(R.color.skyblue);
+        colorcode.add(R.color.color1);
+        colorcode.add(R.color.color2);
+        colorcode.add(R.color.color3);
+        colorcode.add(R.color.color4);
+        colorcode.add(R.color.color5);
+        colorcode.add(R.color.green);
+        Random random = new Random();
+        int number = random.nextInt(colorcode.size());
+        return colorcode.get(number);
     }
 }
