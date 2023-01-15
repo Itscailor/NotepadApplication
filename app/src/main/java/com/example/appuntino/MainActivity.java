e48a365628d2a7445fae2831460ea122a932d622
itscailor <l.cairo@itsrizzoli.it>
2023-01-15 17:49:55 +0100

    Connessione al database e navigazione

--- app/src/main/java/com/example/appuntino/MainActivity.java
+++ app/src/main/java/com/example/appuntino/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.appuntino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText loginemail;
+    private EditText loginpass;
+    private Button login;
+    private Button signup;
+    private TextView nonsignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginemail=findViewById(R.id.loginemail);
+        loginpass=findViewById(R.id.loginpassword);
+        login=findViewById(R.id.Loginbutton);
+        signup=findViewById(R.id.Registerbutton);
+        nonsignup=findViewById(R.id.nonseiid);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,signup.class));
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail=loginemail.getText().toString().trim();
+                String pass=loginpass.getText().toString().trim();
+                if(mail.isEmpty() || pass.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Inserire i dati per continuare",Toast.LENGTH_SHORT).show();
+                }
+                else if(pass.length()<7){
+                    Toast.makeText(getApplicationContext(),"La password deve essere superiore a 7 caratteri",Toast.LENGTH_SHORT).show();
+                }
+                else  {
+                    //login the user
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/appuntino/signup.java
@@ -0,0 +1,92 @@
+package com.example.appuntino;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class signup extends AppCompatActivity {
+    private EditText signupemail;
+    private EditText signuppass;
+    private Button signup;
+    private TextView gologin;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+        getSupportActionBar().hide();
+        signupemail=findViewById(R.id.signupemail);
+        signuppass=findViewById(R.id.signuppassword);
+        signup=findViewById(R.id.Signupbutton);
+        gologin=findViewById(R.id.gotologin);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        gologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signup.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail=signupemail.getText().toString().trim();
+                String pass=signuppass.getText().toString().trim();
+                if(mail.isEmpty() || pass.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Inserire i dati per continuare",Toast.LENGTH_SHORT).show();
+                }
+                else if(pass.length()<7){
+                    Toast.makeText(getApplicationContext(),"La password deve essere superiore a 7 caratteri",Toast.LENGTH_SHORT).show();
+                }
+                else  {
+                    firebaseAuth.createUserWithEmailAndPassword(mail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(),"Registrazione effettuata", Toast.LENGTH_SHORT).show();
+                                sendEmailVerification();
+                            }else{
+                                    Toast.makeText(getApplicationContext(),"Registrazione fallita", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+
+    }
+    private void sendEmailVerification(){
+        FirebaseUser firebaseUser =firebaseAuth.getCurrentUser();
+        if(firebaseUser!=null){
+            firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    Toast.makeText(getApplicationContext(),"Verifica effettuata, procedere con il login", Toast.LENGTH_SHORT).show();
+                    firebaseAuth.signOut();
+                    finish();
+                    startActivity(new Intent(signup.this,MainActivity.class));
+                }
+            });
+
+        }else{
+            Toast.makeText(getApplicationContext(),"Invio della verifica fallito", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
