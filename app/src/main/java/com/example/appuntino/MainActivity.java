cce95683ade41a5dfd37c03d59c9f8034043e6a5
itscailor <l.cairo@itsrizzoli.it>
2023-01-15 18:05:40 +0100

    Controllo utenti e navigazioni finiti

--- app/src/main/java/com/example/appuntino/MainActivity.java
+++ app/src/main/java/com/example/appuntino/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.appuntino;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,22 +11,35 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
     private EditText loginemail;
     private EditText loginpass;
     private Button login;
     private Button signup;
     private TextView nonsignup;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
         loginemail=findViewById(R.id.loginemail);
         loginpass=findViewById(R.id.loginpassword);
         login=findViewById(R.id.Loginbutton);
         signup=findViewById(R.id.Registerbutton);
         nonsignup=findViewById(R.id.nonseiid);
-
+        firebaseAuth=FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser= firebaseAuth.getCurrentUser();
+        if(firebaseUser!=null){
+            finish();
+            startActivity(new Intent(MainActivity.this,notesactivity.class));
+        }
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,8 +59,29 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else  {
                     //login the user
+                    firebaseAuth.signInWithEmailAndPassword(mail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                CheckMailVerification();
+                            }else{
+                                Toast.makeText(getApplicationContext(),"Account non esistente",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
             }
         });
+    }
+    private void CheckMailVerification(){
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+        if(firebaseUser.isEmailVerified()==true){
+            Toast.makeText(getApplicationContext(),"Accesso effettuato",Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(new Intent(MainActivity.this,notesactivity.class));
+        }else {
+            Toast.makeText(getApplicationContext(),"Verifica la tua email per continuare",Toast.LENGTH_SHORT).show();
+            firebaseAuth.signOut();
+        }
     }
 }
